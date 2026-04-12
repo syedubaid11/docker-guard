9fe26028df17ae2cdf7158055d34ab96cc210ec1
syedubaid11 <syedabuubaid@gmail.com>
2026-04-13 00:02:21 +0530

    feat : added scan endpoint

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello from TypeScript Express!');
 });
 
+app.post('/api/scan',(req: Request, res: Response) => {
+  // we will first scan the image if it's the correct format or not only then we will proceeed , also add auth maybe in the future to this?
+  res.send('Scan request received');
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
