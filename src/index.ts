c25a05f524a976602de63abc741a51780b67343a
syedubaid11 <syedabuubaid@gmail.com>
2026-04-12 23:55:42 +0530

    feat : init server

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from TypeScript Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
